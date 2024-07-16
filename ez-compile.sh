c4457580812367af2e4433039465a570d71be30b
Aleksey Shichko <a.i.shichko@tinkoff.ru>
2024-07-17 00:31:05 +0300

    actualize compilation script for multiple locations

--- ez-compile.sh
+++ ez-compile.sh
@@ -1,21 +1,30 @@
-#!/usr/bin/sh
+#!/usr/bin/env bash
 
-filename=${1}
-    # don't generate source maps
-sass_args="--no-source-map"
+# defaults
+import_path=""
 
 usage() {
     echo "Start watching for changes and compile emmidiately"
     echo "filename.scss to filename.css"
-    echo "Usage: ${0} <filename.scss>"
+    echo "Usage: ${0} <filename.scss> [import_path]"
+    echo "  import_path - load-path arg to sass binary"
 }
 
-if [[ -z "$filename" ]]; then
+if [[ -z "$1" ]]; then
     usage
     exit 1
+fi
+filename=${1}
+
+if [ -n "$2" ]; then
+    import_path="${2}"
 fi
 
 # remove the longest suffix starting with "."
 base=${filename%%\.*}
 echo "Compiling $filename to ${base}.css"
-sass ${sass_args} --watch ${filename}:${base}.css
+
+sass \
+    --no-source-map `# don't generate source maps` \
+    --load-path="${import_path}" \
+    --watch ${filename}:${base}.css
